ed3acf365db45be00a2de4789dc7b133b0f14c74
Faizal Chan. <moefc32@gmail.com>
2025-09-11 10:07:45 +0700

    Updates

--- _app/immutable/nodes/2.Ch7nWeDF.js
+++
@@ -1,8 +0,0 @@
-import{f as d,h as u,a as s,s as f}from"../chunks/B_sOT1Xe.js";import{i as h}from"../chunks/BBaCRE_r.js";import{p as v,t as n,a as g,u as t,$ as y,c as o,r,n as x}from"../chunks/C_jl_BMB.js";import{s as w}from"../chunks/B_riCdbT.js";var b=d('<meta property="og:title"/>'),k=d(`<div class="flex flex-1 flex-col gap-3"><h1 class="my-3 pb-3 text-5xl border-b-[1px] border-gray-700"> </h1> <p>Simpledocs is a minimalistic and efficient documentation site designed
-        for designers, developers, and teams who need a quick and organized way
-        to present their documentation. It offers a clean and responsive layout
-        with essential features like a navigation sidebar and search
-        functionality. Built with simplicity in mind, this project ensures that
-        users can focus on writing content without dealing with complex
-        configurations. Its lightweight nature makes it easy to deploy on the
-        GitHub Pages.</p></div>`);function U(l,m){v(m,!1),h();var e=k();u(c=>{var i=b();n(()=>{y.title=t(()=>"Telkom University Brand Guidelines"),w(i,"content",t(()=>"Telkom University Brand Guidelines"))}),s(c,i)});var a=o(e),p=o(a,!0);r(a),x(2),r(e),n(()=>f(p,t(()=>"Telkom University Brand Guidelines"))),s(l,e),g()}export{U as component};
