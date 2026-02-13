5fd13d915a35cd3e7a67fedd27a270c15046f601
Faizal Chan. <moefc32@gmail.com>
2026-02-13 17:47:44 +0700

    Updates

--- _app/immutable/nodes/2.BXs9YzT5.js
+++
@@ -1,8 +0,0 @@
-import"../chunks/Bzak7iHL.js";import{i as c}from"../chunks/CuuK_G7L.js";import{p as u,f as r,h as f,t as a,a as s,b as h,$ as v,c as n,r as o,n as g,s as y}from"../chunks/Bp7gmpXD.js";import{s as x}from"../chunks/hS5PVg1h.js";var b=r('<meta property="og:title"/>'),w=r(`<div class="flex flex-1 flex-col gap-3"><h1 class="my-3 pb-3 text-5xl border-b-[1px] border-gray-700"> </h1> <p>Simpledocs is a minimalistic and efficient documentation site designed
-        for designers, developers, and teams who need a quick and organized way
-        to present their documentation. It offers a clean and responsive layout
-        with essential features like a navigation sidebar and search
-        functionality. Built with simplicity in mind, this project ensures that
-        users can focus on writing content without dealing with complex
-        configurations. Its lightweight nature makes it easy to deploy on the
-        GitHub Pages.</p></div>`);function T(d,l){u(l,!1),c();var e=w();f(m=>{var i=b();a(()=>{v.title="Telkom University Brand Guidelines",x(i,"content","Telkom University Brand Guidelines")}),s(m,i)});var t=n(e),p=n(t,!0);o(t),g(2),o(e),a(()=>y(p,"Telkom University Brand Guidelines")),s(d,e),h()}export{T as component};
---
+++ _app/immutable/nodes/2.CcHRKbnM.js
@@ -0,0 +1,8 @@
+import{a as i,f as r,s as c}from"../chunks/vcfxwmUZ.js";import{i as f}from"../chunks/i4vs1WVr.js";import{p as u,t as s,a as h,d as g,$ as v,c as n,r as o,n as y}from"../chunks/CY0zLpua.js";import{h as x,s as w}from"../chunks/Duk-V3Mu.js";var _=r('<meta property="og:title"/>'),b=r(`<div class="flex flex-1 flex-col gap-3"><h1 class="my-3 pb-3 text-5xl border-b-[1px] border-gray-700"> </h1> <p>Simpledocs is a minimalistic and efficient documentation site designed
+        for designers, developers, and teams who need a quick and organized way
+        to present their documentation. It offers a clean and responsive layout
+        with essential features like a navigation sidebar and search
+        functionality. Built with simplicity in mind, this project ensures that
+        users can focus on writing content without dealing with complex
+        configurations. Its lightweight nature makes it easy to deploy on the
+        GitHub Pages.</p></div>`);function U(d,l){u(l,!1),f();var e=b();x("1uha8ag",p=>{var a=_();s(()=>w(a,"content","Telkom University Brand Guidelines")),g(()=>{v.title="Telkom University Brand Guidelines"}),i(p,a)});var t=n(e),m=n(t,!0);o(t),y(2),o(e),s(()=>c(m,"Telkom University Brand Guidelines")),i(d,e),h()}export{U as component};
